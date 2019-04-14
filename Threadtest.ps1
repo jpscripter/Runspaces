ce351695a66f9ee4103cb8454c0a2e36ae0af88c
JPScripter <JPScripter@Gmail.com>
2019-04-14 14:43:35 -0500

    adding initial files

---
+++ Threadtest.ps1
@@ -0,0 +1,46 @@
+﻿$ScriptBlock = 
+{
+param($times)
+    For($i = 0; $i -lt $times;$i++){
+        [double] $halfpi=1
+        [int] $num=1
+        [double] $factorial=1
+        [double] $oddfactorial=1
+        [double] $pi = 1
+        [double] $previouspi = 0
+
+        while ($previouspi -ne $pi) {
+            $previouspi = $pi
+            $factorial *= $num
+            $oddfactorial *= (($num*2)+1)
+            $halfpi += $factorial / $oddfactorial
+            $pi = 2 * $halfpi
+            $num++
+        }
+    }
+}
+
+foreach ($Threads in (5..30)){
+    $stopwatch = [System.Diagnostics.Stopwatch]::StartNew()
+    $initialSessionState = [initialsessionstate]::CreateDefault() # Enter PSSnapins here
+    $MINRunspaces = 1
+    $MAXRunspaces = $Threads
+    $RunSpacePool = [RunspaceFactory]::CreateRunspacePool($minRunspaces,$MAXRunspaces,$initialSessionState,$Host)
+    #$RunSpacePool.ApartmentState = "MTA" # Thread safe com object coding
+    $RunSpacePool.Open()
+    $times = 10000/$Threads
+    $PowershellArray = @()
+    For ($Counter = 0; $Counter -lt 10; $counter++){
+        $PowerShell = [PowerShell]::Create()
+        $PowerShell.Runspacepool = $RunSpacePool
+        $Null = $PowerShell.AddScript($ScriptBlock)
+        $Null = $PowerShell.AddParameter('Times',$times)
+        $Null = $PowerShell.BeginInvoke()
+        $PowershellArray += $powershell
+    }
+    Start-sleep -Seconds 1
+    While ($PowershellArray.InvocationStateInfo.State -contains 'Running'){
+    }
+    $stopwatch.Stop()
+    "$Threads - $($Stopwatch.Elapsed)"
+}
---
+++ Tricks and Features.ps1
@@ -0,0 +1,151 @@
+﻿#region Initial State
+$initialSessionState = [initialsessionstate]::CreateDefault() # Enter PSSnapins here
+$initialSessionState.LanguageMode = 'Full'
+$initialSessionState.ExecutionPolicy = 'bypass'
+
+#initial Variables
+$Variable = [System.Management.Automation.Runspaces.SessionStateVariableEntry]::new('InitVariable','Value','Description?')
+$initialSessionState.Variables.Add($Variable)
+
+#Initial Modules
+$initialSessionState.ImportPSModule('tls')
+
+#StartupScripts
+$initialSessionState.StartupScripts.Add('$startup = $true')
+
+#Thread Option
+#https://docs.microsoft.com/en-us/dotnet/api/system.management.automation.runspaces.psthreadoptions?view=powershellsdk-1.1.0
+#$initialSessionState.ThreadOptions = 'Default' #'UseCurrentThread'
+#endregion
+
+#region MultiThreaded 
+$MINRunspaces = 1
+$MAXRunspaces = 3
+$RunSpacePool = [RunspaceFactory]::CreateRunspacePool($minRunspaces,$MAXRunspaces,$initialSessionState,$Host)
+#$RunSpacePool.ApartmentState = "MTA" # Thread safe com object coding
+$RunSpacePool.Open()
+#endregion
+
+#region Single Threaded
+#$RunspaceCOnfiguration = [System.Management.Automation.Runspaces.RunspaceConfiguration]::Create() #Console File
+# Similar to InintialSessionState but no thread options
+$Runspace = [RunspaceFactory]::CreateRunspace($Host,$initialSessionState)
+$Runspace.Open()
+#endregion
+
+$hash = @{}
+For ($Counter = 0; $Counter -lt 10; $counter++){
+  #region Basic
+    $PowerShell = [PowerShell]::Create()
+    #$PowerShell.Runspacepool = $RunSpacePool
+    $PowerShell.Runspace = $RunSpace
+    
+    $ScriptBlock = {
+        $Process = Start-Process $Env:comspec -PassThru 
+        Write-output -InputObject 'Runspaces!'
+        Start-sleep -Seconds 20
+        stop-process -InputObject $process
+    }
+    $Null = $PowerShell.AddScript($ScriptBlock)
+    $Null = $PowerShell.BeginInvoke()
+
+    <#region Extracting data
+    Start-sleep -Seconds 2
+    $Monitor = $powershell.CreateNestedPowerShell()
+    $Monitor.AddScript({
+        $VerbosePreference = 2
+        Write-Verbose $process}
+    )
+    $Monitor.Invoke()
+    #endregion
+    #>
+
+    #region streams
+    $Powershell.streams
+    $Flags = 'nonpublic','instance','static'
+    [Powershell].getProperty('OutputBuffer',$Flags).getValue($PowerShell)
+    #endregion
+
+  #endregion
+
+  #region Helpful
+  $Hash.Add($Counter,$PowerShell)
+  $Counter
+  #endregion
+}
+
+
+#region Syncronized hash tables
+$initialSessionState = [initialsessionstate]::CreateDefault() # Enter PSSnapins here
+$initialSessionState.LanguageMode = 'Full'
+$initialSessionState.ExecutionPolicy = 'bypass'
+$hash = [hashtable]::Synchronized(@{})
+$Variable = [System.Management.Automation.Runspaces.SessionStateVariableEntry]::new('SyncHash',$hash ,'Description?')
+$initialSessionState.Variables.Add($Variable)
+
+$Runspace = [RunspaceFactory]::CreateRunspace($Host,$initialSessionState)
+$Runspace.Open()
+
+$PowerShell = [PowerShell]::Create()
+$PowerShell.Runspace = $RunSpace
+$Hash.add('name','value')
+$ScriptBlock = {
+    $VerbosePreference = 2
+    Write-Verbose  $SyncHash['name']
+    $SyncHash['name'] = 'Changed'
+    start-sleep -Seconds 3 
+}
+$Null = $PowerShell.AddScript($ScriptBlock)
+$Null = $PowerShell.BeginInvoke()
+$hash
+#endregion
+
+
+#region Debugging
+$initialSessionState = [initialsessionstate]::CreateDefault() # Enter PSSnapins here
+$initialSessionState.LanguageMode = 'Full'
+$initialSessionState.ExecutionPolicy = 'bypass'
+$Variable = [System.Management.Automation.Runspaces.SessionStateVariableEntry]::new('InitVariable','Value','Description?')
+$initialSessionState.Variables.Add($Variable)
+
+$Runspace = [RunspaceFactory]::CreateRunspace($Host,$initialSessionState)
+$Runspace.Open()
+
+$PowerShell = [PowerShell]::Create()
+$PowerShell.Runspace = $RunSpace
+$ScriptBlock = {
+    Write-output -InputObject $InitVariable
+    start-sleep -Seconds 3 
+}
+$Null = $PowerShell.AddScript($ScriptBlock)
+$Null = $PowerShell.BeginInvoke()
+Debug-Runspace -Runspace $RunSpace 
+
+#debug-job for background jobs
+#endregion
+
+#region Remote Runspace
+#https://docs.microsoft.com/en-us/powershell/developer/hosting/creating-remote-runspaces
+$connectionInfo = [System.Management.Automation.Runspaces.WSManConnectionInfo]::new
+$Runspace = [runspacefactory]::CreateRunspace($connectionInfo)
+#endregion
+
+#region Changing credentials
+$Signature = @'
+[DllImport("advapi32.dll", SetLastError = true, CharSet=CharSet.Unicode)]
+public static extern bool LogonUserW(
+  string pszUserName,
+  string pszDomain,
+  string pszPassword,
+  int dwLogonType,
+  int dwLogonProvider,
+  ref IntPtr phToken);
+'@
+$Logonuser = Add-Type -MemberDefinition $Signature -Name LogonUserW -PassThru
+[intptr] $token = 0
+$Credential = Get-Credential 
+$Logonuser::LogonUserw($Credential.GetNetworkCredential().UserName,$Credential.GetNetworkCredential().Domain,$Credential.GetNetworkCredential().Password,9,0,[ref]$Token)
+[System.Security.Principal.WindowsIdentity]::Impersonate($token)
+[System.Security.Principal.WindowsIdentity]::GetCurrent()
+[System.Security.Principal.WindowsIdentity]::Impersonate(0)
+#endregion
